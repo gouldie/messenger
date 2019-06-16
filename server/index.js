050b9cf5b552d72d7cc2a2a35f88a6c5f14e5b4f
Matt <mgould@breakingfreegroup.com>
2019-06-16 14:52:54 +0100

    setup

---
+++ server/index.js
@@ -0,0 +1,19 @@
+import { ApolloServer, gql } from 'apollo-server'
+
+const PORT = 8080
+
+// schema
+const typeDefs = gql`
+  type Query {
+    testString: String
+  }
+`
+
+const server = new ApolloServer({
+  typeDefs,
+  mocks: true
+})
+
+server.listen({ port: PORT }).then(({url}) => {
+  console.log(`Server ready at ${url}`)
+})
